7ec196c68e3b51408dd2f93dbff4fd4d90876454
harsha <harshavardhanreddy159@gmail.com>
2024-03-09 12:59:19 +0530

    commit

--- class1/2.js
+++
@@ -1,6 +0,0 @@
-let x=require('./1')
-
-let b=100+x.a;
-
-console.log(b);
-console.log(x)
--- class1/2.mjs
+++
@@ -1,6 +0,0 @@
-
-import {a,username} from './1.js'
-let b=100+a;
-
-console.log(b);
-console.log(username)
